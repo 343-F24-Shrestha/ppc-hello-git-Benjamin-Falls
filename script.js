2d7b25356909418009cbb76dd6da8e0e5f13038e
Benjamin-Falls <fallsbh@dukes.jmu.edu>
2026-03-12 16:08:08 -0400

    Profile picture and button functionality

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ let contact = document.getElementById("contact");
 contact.addEventListener("click", sayHello);
 
 function sayHello() {
-  alert("Under Construction - Thanks for visiting my profile!");
+  let button = document.getElementById("contact");
+  let email = document.getElementById("email");
+  if (email.textContent == "") {
+    email.textContent = "fallsbh@dukes.jmu.edu";
+    email.style.background = "yellow";
+    email.hidden = true;
+  }
+  if (email.hidden == false) {
+    email.toggleAttribute("hidden");
+    button.textContent = "Say Hello!";
+  } else {
+    email.toggleAttribute("hidden");
+    button.textContent = "Hide!";
+  }
+
+
 }
